f3e4ba0eb1980b0c82748c9506891b14747b93b4
Pietro Di Bello <pierodibello@gmail.com>
2024-03-14 22:04:07 +0100

    feat: read a simple ROS file with multiple entries

--- src/test/kotlin/grocerystore/CalculateROSGrandTotalIncomeTest.kt
+++ src/test/kotlin/grocerystore/CalculateROSGrandTotalIncomeTest.kt
@@ -7,16 +7,19 @@ import java.io.File
 class CalculateROSGrandTotalIncomeTest {
 
     @Test
-    fun `read a simple ROS file with just one entry`() {
+    fun `read a simple ROS file with multiple entries`() {
         val rosFile = File.createTempFile("ros", ".txt").apply {
             writeText("""
                 bread, 1, 2
+                12-pack of eggs, 1, 2
+                milk (1L), 4, 8
+                coca cola (33cl), 10, 10
                 """.trimIndent())
         }.also { it.deleteOnExit() }
 
         val gti = RecordOfSales(rosFile.absolutePath).computeGrandTotalIncome()
 
-        gti shouldBe 1 * 2
+        gti shouldBe (1 * 2) + (1 * 2) + (4 * 8) + (10 * 10)
     }
 }
 
