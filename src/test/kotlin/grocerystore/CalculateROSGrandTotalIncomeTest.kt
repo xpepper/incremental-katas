839691c86d655c088113cbbaaa918e0ea8e99961
Pietro Di Bello <pierodibello@gmail.com>
2024-03-14 22:08:41 +0100

    refactor(test): extract common test helper to create a ROS file

--- src/test/kotlin/grocerystore/CalculateROSGrandTotalIncomeTest.kt
+++ src/test/kotlin/grocerystore/CalculateROSGrandTotalIncomeTest.kt
@@ -8,29 +8,32 @@ class CalculateROSGrandTotalIncomeTest {
 
     @Test
     fun `gti is zero if the ros file is empty`() {
-        val rosFile = File.createTempFile("ros", ".txt").apply {
-            writeText("".trimIndent())
-        }.also { it.deleteOnExit() }
+        val emptyRosFile = writeRosFileWith("")
 
-        val gti = RecordOfSales(rosFile.absolutePath).computeGrandTotalIncome()
+        val gti = RecordOfSales(emptyRosFile.absolutePath).computeGrandTotalIncome()
 
         gti shouldBe 0
     }
 
+
     @Test
     fun `compute gti for a single ROS file with multiple entries`() {
-        val rosFile = File.createTempFile("ros", ".txt").apply {
-            writeText("""
-                bread, 1, 2
-                12-pack of eggs, 1, 2
-                milk (1L), 4, 8
-                coca cola (33cl), 10, 10
-                """.trimIndent())
-        }.also { it.deleteOnExit() }
+        val rosFile = writeRosFileWith(
+            """
+            bread, 1, 2
+            12-pack of eggs, 1, 2
+            milk (1L), 4, 8
+            coca cola (33cl), 10, 10
+            """
+        )
 
         val gti = RecordOfSales(rosFile.absolutePath).computeGrandTotalIncome()
 
         gti shouldBe (1 * 2) + (1 * 2) + (4 * 8) + (10 * 10)
     }
+
+    private fun writeRosFileWith(fileContent: String) = File.createTempFile("ros", ".txt").apply {
+        writeText(fileContent.trimIndent())
+    }.also { it.deleteOnExit() }
 }
 
