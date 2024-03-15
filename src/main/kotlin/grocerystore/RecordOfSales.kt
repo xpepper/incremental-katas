ea25e5ffdd5bdef1653ed28a5ce519c7ed51c641
Pietro Di Bello <pierodibello@gmail.com>
2024-03-15 22:45:34 +0100

    refactor: remove duplication in computing the grand total income

--- src/main/kotlin/grocerystore/RecordOfSales.kt
+++ src/main/kotlin/grocerystore/RecordOfSales.kt
@@ -7,7 +7,7 @@ private val quantityAndPriceRegex = Regex("""(.+),\s*(\d+),\s*(\d+)${'$'}""")
 class RecordOfSales(rosFilePath: String) {
     private val entries: List<Entry> = parseToEntries(File(rosFilePath))
 
-    fun computeGrandTotalIncome(): Long = entries.sumOf { entry -> entry.quantity * entry.price }
+    fun computeGrandTotalIncome(): Long = entries.grantTotalIncome()
 
     private fun parseToEntries(rosFile: File): List<Entry> = rosFile.readLines().map(::parse)
 
@@ -22,16 +22,14 @@ class RecordOfSales(rosFilePath: String) {
 
     fun withCategories(rawCategories: String) = CategorizedRecordOfSales(this, rawCategories)
 
-    private data class Entry(val product: ProductItem, val quantity: Long, val price: Long)
 
     class CategorizedRecordOfSales(private val recordOfSales: RecordOfSales, private val rawCategories: String) {
         fun generateReport(): String = categoriesFrom(rawCategories).joinToString("\n") { (productFamily, category) ->
             recordOfSales.entries
-                .filter { (product, _, _) -> product.belongsTo(productFamily) }
-                .sumOf { (_, quantity, price) -> quantity * price }
+                .filter { entry -> entry.product.belongsTo(productFamily) }
+                .grantTotalIncome()
                 .let { categoryTotal -> "${category.name}: $categoryTotal" }
         } + "\ntotal: ${recordOfSales.computeGrandTotalIncome()}"
-
 
         private fun categoriesFrom(rawCategories: String): List<Pair<ProductFamily, Category>> =
             rawCategories.split("\n")
@@ -39,8 +37,14 @@ class RecordOfSales(rosFilePath: String) {
                 .filter { it.isNotEmpty() }
                 .map { it.split(", ") }
                 .map { ProductFamily(it[0]) to Category(it[1]) }
+
+
     }
 }
+
+private data class Entry(val product: ProductItem, val quantity: Long, val price: Long)
+
+private fun List<Entry>.grantTotalIncome() = sumOf { (_, quantity, price) -> quantity * price }
 
 @JvmInline
 value class Category(internal val name: String)
@@ -52,3 +56,4 @@ value class ProductFamily(internal val name: String)
 value class ProductItem(private val name: String) {
     fun belongsTo(productFamily: ProductFamily) = name.contains(productFamily.name)
 }
+
