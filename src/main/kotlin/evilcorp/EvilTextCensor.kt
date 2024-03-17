3a57178e45677b9a90610de1a92d8e5dd739c14a
Pietro Di Bello <pierodibello@gmail.com>
2024-03-17 22:06:10 +0100

    refactor(evilcorp): extract function

--- src/main/kotlin/evilcorp/EvilTextCensor.kt
+++ src/main/kotlin/evilcorp/EvilTextCensor.kt
@@ -2,6 +2,8 @@ package evilcorp
 
 class EvilTextCensor(private val blacklistedWords: Set<String>) {
     fun censor(text: String): String = blacklistedWords.fold(text) { censoredText, blacklistedWord ->
-        censoredText.replace(blacklistedWord, "X".repeat(blacklistedWord.length))
+        censoredText.replace(blacklistedWord, blacklistedWord.toCensoredVersion())
     }
+
+    private fun String.toCensoredVersion() = "X".repeat(length)
 }
